e75d33f0ffc2f6b6c7ae7895686bd224c2ed9738
ylqer <617093056@qq.com>
2021-01-06 22:40:29 +0800

    add 16_binary_tree.py

--- 16_binary_tree.py
+++ 16_binary_tree.py
@@ -21,12 +21,12 @@ class Tree(object):
 
         while queue:
             cur_node = queue.pop(0)
-            if cur_node.l_child is not None:
+            if cur_node.l_child is None:
                 cur_node.l_child = node
                 return
             else:
                 queue.append(cur_node.l_child)
-            if cur_node.r_child is not None:
+            if cur_node.r_child is None:
                 cur_node.r_child = node
                 return
             else:
@@ -40,10 +40,53 @@ class Tree(object):
         queue = [self.root]
         while queue:
             cur_node = queue.pop(0)
-            print(cur_node.elem)
+            print(cur_node.elem, end=' ')
             if cur_node.l_child is not None:
                 queue.append(cur_node.l_child)
             if cur_node.r_child is not None:
                 queue.append(cur_node.r_child)
 
+    def preorder(self, node):
+        """先序遍历"""
+        if node is None:
+            return
+        print(node.elem, end=' ')
+        self.preorder(node.l_child)
+        self.preorder(node.r_child)
 
+    def inorder(self, node):
+        """中序遍历"""
+        if node is None:
+            return
+        self.inorder(node.l_child)
+        print(node.elem, end=' ')
+        self.inorder(node.r_child)
+
+    def postorder(self, node):
+        """后序遍历"""
+        if node is None:
+            return
+        self.postorder(node.l_child)
+        self.postorder(node.r_child)
+        print(node.elem, end=' ')
+
+
+if __name__ == '__main__':
+    tree = Tree()
+    tree.add(0)
+    tree.add(1)
+    tree.add(2)
+    tree.add(3)
+    tree.add(4)
+    tree.add(5)
+    tree.add(6)
+    tree.add(7)
+    tree.add(8)
+    tree.add(9)
+    tree.breadth_travel()
+    print(' ')
+    tree.preorder(tree.root)
+    print('')
+    tree.inorder(tree.root)
+    print('')
+    tree.postorder(tree.root)
