d0e15f1d9ddf7593f2ab5d5740cc8df659fdf04e
ylqer <617093056@qq.com>
2021-01-09 10:43:10 +0800

    add 02_list.py

--- 02_list.py
+++ 02_list.py
@@ -1,3 +1,53 @@
-import timeit
+from timeit import Timer
 
-li = []
+li1 = [1, 2]
+
+# li2 = [23, 5]
+#
+# li = li1 + li2
+#
+# li = [i for i in range(10000)]
+#
+# li = list(range(10000))
+
+
+def test1():
+    li = []
+    for i in range(10000):
+        li.append(i)
+
+
+def test2():
+    li = []
+    for i in range(10000):
+        li = li + [i]
+
+
+def test3():
+    li = [i for i in range(10000)]
+
+
+def test4():
+    li = list(range(10000))
+
+
+def test5():
+    li = []
+    for i in range(10000):
+        li.extend([i])
+
+
+timer1 = Timer('test1()', 'from __main__ import test1')
+print("append:", timer1.timeit(1000))
+
+timer2 = Timer('test2()', 'from __main__ import test2')
+print("+:", timer2.timeit(1000))
+
+timer3 = Timer('test3()', 'from __main__ import test3')
+print("[i for i in range]:", timer3.timeit(1000))
+
+timer4 = Timer('test4()', 'from __main__ import test4')
+print("list(range()):", timer4.timeit(1000))
+
+timer5 = Timer('test5()', 'from __main__ import test5')
+print("list(range()):", timer5.timeit(1000))
